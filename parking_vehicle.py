6b9dc5ea21495ee3355cd671428182a6dc308a88
EverettRichards <ehrichards9@gmail.com>
2024-07-25 17:07:53 -0400

    Final for the day

--- parking_vehicle.py
+++ parking_vehicle.py
@@ -77,7 +77,6 @@ def on_message(client, userdata, msg):
     # Turn from string text to data structure
     payload = decodePayload(payload)
     # Handle the message
-    print(msg.topic)
     if msg.topic == "verdict":
         # Receive a verdict from the server. Utilize it.
         processVerdict(payload)

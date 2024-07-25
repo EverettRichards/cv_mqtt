540073d9ee1b4b04a4909bc45448064e439e3694
EverettRichards <ehrichards9@gmail.com>
2024-07-25 12:50:12 -0400

    Fixed ref to angles for client

--- vehicle.py
+++ vehicle.py
@@ -127,7 +127,7 @@ def ComputerVision():
     for obj in object_locations.keys():
         obj_loc = object_locations[obj]
         theta = np.arctan2(obj_loc["y"]-my_loc["y"],obj_loc["x"]-my_loc["x"]).item()
-        angles_to_each_object[obj] = my_loc["theta"] - theta * 180 / np.pi
+        angles_to_each_object[obj] = (my_loc["car_angle"]-my_loc["camera_angle"]) - theta * 180 / np.pi
 
     horizontal_angle_per_pixel = config["horizontal_FOV"] / config["image_width"]
     #vertical_angle_per_pixel = config["vertical_FOV"] / config["image_height"]

97d36eeaef1960ff9f129703a4c3ca78118dea1a
EverettRichards <ehrichards9@gmail.com>
2024-07-29 12:19:24 -0400

    Fixed my_loc again

--- consolidated_vehicle.py
+++ consolidated_vehicle.py
@@ -286,7 +286,7 @@ def MainLoop():
     initial_vehicle_orientation = (vehicle_locations[client_name]["car_angle"] - vehicle_locations[client_name]["camera_angle"]) % 360
     moveCameraToAngle(px,vehicle_locations[client_name]["camera_angle"])
     current_vehicle_orientation = initial_vehicle_orientation
-    my_loc = current_vehicle_location # Temp dual-reference variable
+    my_loc = vehicle_locations[client_name] # Temp dual-reference variable
 
     angles_to_each_object = {} # Strictly in Degrees. The expected angles from the robot to each of the listed object locations.
 
